8e44ee07cb03097378c3fea7bc2a68d5aeea70fa
Anna Artemova <annartmva@gmail.com>
2025-09-12 12:31:05 +0200

    fix: related stories layout

--- src/components/pages/customers/story/related.tsx
+++ src/components/pages/customers/story/related.tsx
@@ -14,29 +14,29 @@ export default function Related({
         Explore
       </h2>
       <ul className="flex flex-col overflow-hidden rounded-[8px] border border-gray-3">
-        {customers?.map((customer) => (
-          <li
-            key={customer._id}
-            className="relative flex flex-col gap-y-2.5 border-b border-gray-3 p-4 pb-3.5 transition-colors duration-200 last:border-b-0 hover:bg-gray-3"
-          >
-            {(customer.slug || customer.url) && (
+        {customers?.map((customer) => {
+          if (!customer.slug && !customer.url) return null
+
+          return (
+            <li key={customer._id}>
               <Link
-                className="absolute top-0 left-0 h-full w-full"
+                className="flex flex-col items-start gap-y-2.5 border-b border-gray-3 p-4 pb-3.5 transition-colors duration-200 last:border-b-0 hover:bg-gray-3"
                 href={
                   customer.slug
                     ? `${ROUTE.customers}/${customer.slug.current}`
                     : customer.url!
                 }
-              />
-            )}
-            <p className="text-sm leading-none font-medium tracking-tighter text-lagune-3">
-              Customer Story
-            </p>
-            <h3 className="text-base leading-snug font-medium tracking-tighter md:text-[20px]">
-              {customer.title}
-            </h3>
-          </li>
-        ))}
+              >
+                <span className="text-sm leading-none font-medium tracking-tighter text-lagune-3 capitalize">
+                  Customer story
+                </span>
+                <span className="text-base leading-snug font-medium tracking-tighter text-white md:text-[20px]">
+                  {customer.title}
+                </span>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
