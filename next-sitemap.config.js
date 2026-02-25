1aea2d09073fd5871addc3fa56e1132f87a7e65e
Pavel Vinogradov <vin.pavel13@gmail.com>
2026-02-25 16:52:27 +0300

    fix: update canonicals urls for SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || "http://localhost:3000",
+  trailingSlash: true,
   generateRobotsTxt: false,
   sitemapBaseFileName: "next-sitemap",
   exclude: ["/studio", "/studio/*", "/api/*"],
