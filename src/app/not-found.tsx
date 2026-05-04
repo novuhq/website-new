b433939dcc744076bf6705db1862af5430a4cf02
Ivan Smirnov <smirnovrichi@gmail.com>
2026-05-04 18:30:56 +0300

    fix: add website level not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-import NextLink from "next/link"
-import { ROUTE } from "@/constants/routes"
 import { SEO_DATA } from "@/constants/seo-data"
 import { Providers } from "@/contexts"
 
@@ -8,10 +6,10 @@ import {
   getLatestBlogPost,
   getLatestChangelogPost,
 } from "@/lib/get-header-data"
-import { Button } from "@/components/ui/button"
 import Fonts from "@/components/fonts"
 import Footer from "@/components/footer"
 import Header from "@/components/header"
+import NotFoundContent from "@/components/pages/not-found"
 import Scripts, { GTM_ID } from "@/components/scripts"
 
 export default async function NotFound() {
@@ -41,21 +39,7 @@ export default async function NotFound() {
         <Providers>
           <Header githubStars={stars} changelog={changelog} blog={blog} />
           <main className="flex grow">
-            <section className="not-found flex grow items-center justify-center px-5 py-20 md:px-8">
-              <div className="flex max-w-md flex-col items-center justify-center md:max-w-lg">
-                <h1 className="text-8xl leading-none font-semibold tracking-tighter text-foreground md:text-9xl md:leading-none">
-                  <span className="sr-only">Error</span>404
-                  <span className="sr-only">: Page Not Found</span>
-                </h1>
-                <p className="mt-2.5 text-center text-base leading-normal tracking-tight text-foreground md:text-lg md:leading-normal">
-                  We know this isn&apos;t where you intended to land, but we
-                  hope you have some fun while you&apos;re here.
-                </p>
-                <Button className="mt-6 xl:mt-8" variant="outline" asChild>
-                  <NextLink href={ROUTE.index}>Go back home</NextLink>
-                </Button>
-              </div>
-            </section>
+            <NotFoundContent />
           </main>
           <Footer />
         </Providers>
